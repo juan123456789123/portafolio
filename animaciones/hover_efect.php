5a437606101657e85fa719c597b0223532c90986
keren <juan.jcn4123@gmail.com>
2023-01-16 16:59:14 -0600

    pruebas

---
+++ animaciones/hover_efect.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+
+
+</head>
+<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+    <body>
+        
+    <div class="container">
+        <div class="btn"><a href="#">Youtube</a></div>
+        <div class="btn"><a href="#">Facebook</a></div>
+
+        <div class="btn"><a href="#">WhatsApp</a></div>
+
+    </div>
+    </body>
+    
+</body>
+</html>
---
+++ animaciones/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login Now | Nothing</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/solid.css" integrity="sha384-Tv5i09RULyHKMwX0E8wJUqSOaXlyu3SQxORObAI08iUwIalMmN5L6AvlPX2LMoSE" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+
+
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="screen">
+
+            <div class="screen_content">
+                <form action="" class="login">
+                    <div class="login_field">
+                        <i class="login_icon fas fa-user"></i>
+                        <input type="text" class="login_input" placeholder="user_name / E-mail">
+                    </div>
+                    <div class="login_field">
+                        <i class="login_icon fas fa-lock"></i>
+                        <input type="text" class="login_input" placeholder="password">
+                    </div>
+                    <button class="button login_submit">
+                        <span class="button_text">Log In Now</span>
+                        <i class="button_icon"></i>
+                    </button>
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
